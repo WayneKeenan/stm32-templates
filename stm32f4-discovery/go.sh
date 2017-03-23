5d0fac2ad7714ab7f31c944c2843a61cc28c0007
Wayne Keenan <wayne.keenan@gmail.com>
2017-03-23 06:27:32 +0000

    updated for DTMHAL lib

---
+++ stm32f4-discovery/go.sh
@@ -0,0 +1,3 @@
+make clean && make all && st-flash erase && st-flash write main.bin 0x08000000 && st-util -p 3333
+
+
--- stm32f4-discovery/src/main.c
+++ stm32f4-discovery/src/main.c
@@ -56,43 +56,43 @@ int main(void)
      */
 
   /* GPIOD Periph clock enable */
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 
   /* Configure PD12, PD13, PD14 and PD15 in output pushpull mode */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_5;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-  GPIO_Init(GPIOD, &GPIO_InitStructure);
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
 
   while (1)
   {
     /* PD12 to be toggled */
-    GPIO_SetBits(GPIOD, GPIO_Pin_12);
+    GPIO_SetBits(GPIOA, GPIO_Pin_12);
     
     /* Insert delay */
     Delay(0x3FFFFF);
     
     /* PD13 to be toggled */
-    GPIO_SetBits(GPIOD, GPIO_Pin_13);
+    GPIO_SetBits(GPIOA, GPIO_Pin_13);
     
     /* Insert delay */
     Delay(0x3FFFFF);
   
     /* PD14 to be toggled */
-    GPIO_SetBits(GPIOD, GPIO_Pin_14);
+    GPIO_SetBits(GPIOA, GPIO_Pin_14);
     
     /* Insert delay */
     Delay(0x3FFFFF);
     
     /* PD15 to be toggled */
-    GPIO_SetBits(GPIOD, GPIO_Pin_15);
+    GPIO_SetBits(GPIOA, GPIO_Pin_5);
     
     /* Insert delay */
     Delay(0x7FFFFF);
     
-    GPIO_ResetBits(GPIOD, GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15);
+    GPIO_ResetBits(GPIOA, GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_5);
     
     /* Insert delay */
     Delay(0xFFFFFF);
